cdd56de8203c8e0cbbde59d6688110464034d806
n4l5u0r <59141606+n4l5u0r@users.noreply.github.com>
2021-11-16 14:50:30 +0100

    feat: initial import of boilerplate template

---
+++ src/handle_provide_parameter.c
@@ -0,0 +1,86 @@
+#include "boilerplate_plugin.h"
+
+// Copies the whole parameter (32 bytes long) from `src` to `dst`.
+// Useful for numbers, data...
+static void copy_parameter(uint8_t *dst, size_t dst_len, uint8_t *src) {
+    // Take the minimum between dst_len and parameter_length to make sure we don't overwrite memory.
+    size_t len = MIN(dst_len, PARAMETER_LENGTH);
+    memcpy(dst, src, len);
+}
+
+// Copies a 20 byte address (located in a 32 bytes parameter) `from `src` to `dst`.
+// Useful for token addresses, user addresses...
+static void copy_address(uint8_t *dst, size_t dst_len, uint8_t *src) {
+    // An address is 20 bytes long: so we need to make sure we skip the first 12 bytes!
+    size_t offset = PARAMETER_LENGTH - ADDRESS_LENGTH;
+    size_t len = MIN(dst_len, ADDRESS_LENGTH);
+    memcpy(dst, &src[offset], len);
+}
+
+// EDIT THIS: Remove this function and write your own handlers!
+static void handle_swap_exact_eth_for_tokens(ethPluginProvideParameter_t *msg, context_t *context) {
+    if (context->go_to_offset) {
+        if (msg->parameterOffset != context->offset + SELECTOR_SIZE) {
+            return;
+        }
+        context->go_to_offset = false;
+    }
+    switch (context->next_param) {
+        case MIN_AMOUNT_RECEIVED:  // amountOutMin
+            copy_parameter(context->amount_received,
+                           sizeof(context->amount_received),
+                           msg->parameter);
+            context->next_param = PATH_OFFSET;
+            break;
+        case PATH_OFFSET:  // path
+            context->offset = U2BE(msg->parameter, PARAMETER_LENGTH - 2);
+            context->next_param = BENEFICIARY;
+            break;
+        case BENEFICIARY:  // to
+            copy_address(context->beneficiary, sizeof(context->beneficiary), msg->parameter);
+            context->next_param = PATH_LENGTH;
+            context->go_to_offset = true;
+            break;
+        case PATH_LENGTH:
+            context->offset = msg->parameterOffset - SELECTOR_SIZE + PARAMETER_LENGTH * 2;
+            context->go_to_offset = true;
+            context->next_param = TOKEN_RECEIVED;
+            break;
+        case TOKEN_RECEIVED:  // path[1] -> contract address of token received
+            copy_address(context->token_received, sizeof(context->token_received), msg->parameter);
+            context->next_param = UNEXPECTED_PARAMETER;
+            break;
+        // Keep this
+        default:
+            PRINTF("Param not supported: %d\n", context->next_param);
+            msg->result = ETH_PLUGIN_RESULT_ERROR;
+            break;
+    }
+}
+
+void handle_provide_parameter(void *parameters) {
+    ethPluginProvideParameter_t *msg = (ethPluginProvideParameter_t *) parameters;
+    context_t *context = (context_t *) msg->pluginContext;
+    // We use `%.*H`: it's a utility function to print bytes. You first give
+    // the number of bytes you wish to print (in this case, `PARAMETER_LENGTH`) and then
+    // the address (here `msg->parameter`).
+    PRINTF("plugin provide parameter: offset %d\nBytes: %.*H\n",
+           msg->parameterOffset,
+           PARAMETER_LENGTH,
+           msg->parameter);
+
+    msg->result = ETH_PLUGIN_RESULT_OK;
+
+    // EDIT THIS: adapt the cases and the names of the functions.
+    switch (context->selectorIndex) {
+        case SWAP_EXACT_ETH_FOR_TOKENS:
+            handle_swap_exact_eth_for_tokens(msg, context);
+            break;
+        case BOILERPLATE_DUMMY_2:
+            break;
+        default:
+            PRINTF("Selector Index not supported: %d\n", context->selectorIndex);
+            msg->result = ETH_PLUGIN_RESULT_ERROR;
+            break;
+    }
+}
