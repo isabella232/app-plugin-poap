1b75b072aa06dcdf267f90ea641105d74fbbd330
lisaoulmi <88668098+lisaoulmi@users.noreply.github.com>
2021-11-22 15:16:33 +0100

    feat(poap ledger plugin): added mintToken method to poap plugin

--- src/handle_query_contract_id.c
+++ src/handle_query_contract_id.c
@@ -1,21 +1,15 @@
 #include "poap_plugin.h"
 
-// Sets the first screen to display.
 void handle_query_contract_id(void *parameters) {
     ethQueryContractID_t *msg = (ethQueryContractID_t *) parameters;
     context_t *context = (context_t *) msg->pluginContext;
-    // msg->name will be the upper sentence displayed on the screen.
-    // msg->version will be the lower sentence displayed on the screen.
 
-    // For the first screen, display the plugin name.
     strlcpy(msg->name, PLUGIN_NAME, msg->nameLength);
 
-    // EDIT THIS: Adapt the cases by modifying the strings you pass to `strlcpy`.
     switch (context->selectorIndex) {
-        case SWAP_EXACT_ETH_FOR_TOKENS:
-            strlcpy(msg->version, "Swap", msg->versionLength);
+        case MINT_TOKEN:
+            strlcpy(msg->version, "Mint", msg->versionLength);
             break;
-        // Keep this
         default:
             PRINTF("Selector index: %d not supported\n", context->selectorIndex);
             msg->result = ETH_PLUGIN_RESULT_ERROR;
--- src/poap_plugin.h
+++ src/poap_plugin.h
@@ -4,53 +4,61 @@
 #include "eth_plugin_interface.h"
 #include <string.h>
 
-// Number of selectors defined in this plugin. Should match the enum `selector_t`.
-// EDIT THIS: Put in the number of selectors your plugin is going to support.
-#define NUM_SELECTORS 2
 
-// Name of the plugin.
-// EDIT THIS: Replace with your plugin name.
+#define NUM_SELECTORS 1
+
 #define PLUGIN_NAME "Poap"
 
-// Enumeration of the different selectors possible.
-// Should follow the exact same order as the array declared in main.c
-// EDIT THIS: Change the naming (`selector_t`), and add your selector names.
+#define TOKEN_RECEIVED_FOUND 1 << 1
+
+#define SELECTOR_SIZE 4
+
+#define PARAMETER_LENGTH 32
+
+#define RUN_APPLICATION 1
+
 typedef enum {
-    SWAP_EXACT_ETH_FOR_TOKENS = 0,
-    POAP_DUMMY_2,
+    MINT_TOKEN,
 } selector_t;
 
 // Enumeration used to parse the smart contract data.
-// EDIT THIS: Adapt the parameter names here.
 typedef enum {
-    MIN_AMOUNT_RECEIVED = 0,
+    EVENT_ID,
     TOKEN_RECEIVED,
     BENEFICIARY,
-    PATH_OFFSET,
-    PATH_LENGTH,
-    UNEXPECTED_PARAMETER,
+    NONE,
 } parameter;
 
-// EDIT THIS: Rename `POAP` to be the same as the one initialized in `main.c`.
+typedef enum {
+    MINT_SCREEN,
+    TOKEN_SCREEN,
+    WARN_SCREEN,
+    BENEFICIARY_SCREEN,
+    ERROR,
+} screens_t;
+
 extern const uint8_t *const POAP_SELECTORS[NUM_SELECTORS];
 
+// Number of decimals used when the token wasn't found in the CAL.
+#define DEFAULT_DECIMAL WEI_TO_ETHER
+
+// Ticker used when the token wasn't found in the CAL.
+#define DEFAULT_TICKER ""
+
 // Shared global memory with Ethereum app. Must be at most 5 * 32 bytes.
-// EDIT THIS: This struct is used by your plugin to save the parameters you parse. You
-// will need to adapt this struct to your plugin.
 typedef struct context_t {
     // For display.
-    uint8_t amount_received[INT256_LENGTH];
     uint8_t beneficiary[ADDRESS_LENGTH];
     uint8_t token_received[ADDRESS_LENGTH];
     char ticker[MAX_TICKER_LEN];
     uint8_t decimals;
-    uint8_t token_found;
 
     // For parsing data.
-    uint8_t next_param;  // Set to be the next param we expect to parse.
-    uint16_t offset;     // Offset at which the array or struct starts.
-    bool go_to_offset;   // If set, will force the parsing to iterate through parameters until
-                         // `offset` is reached.
+    uint16_t offset;
+    uint16_t checkpoint;
+    uint8_t skip;
+    uint8_t next_param;
+    uint8_t tokens_found;
 
     // For both parsing and display.
     selector_t selectorIndex;
