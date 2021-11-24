e07e3a9878ad1ce8fd221d26aa70597fed21de33
lisaoulmi <88668098+lisaoulmi@users.noreply.github.com>
2021-11-24 10:00:26 +0100

    ledger plugin(mint token): implemented mint token tests env (unstable version)

--- src/handle_query_contract_ui.c
+++ src/handle_query_contract_ui.c
@@ -1,60 +1,81 @@
 #include "poap_plugin.h"
 
-// Set UI for "Mint" screen.
-static void set_mint_ui(ethQueryContractUI_t *msg, context_t *context) {
-    strlcpy(msg->title, "Mint", msg->titleLength);
-    strlcpy(msg->msg, "POAP", msg->msgLength);
+// Set UI for the "Send" screen.
+static void set_send_ui(ethQueryContractUI_t *msg, poap_parameters_t *context) {
+    switch (context->selectorIndex) {
+        case MINT_TOKEN:
+            strlcpy(msg->title, "Mint", msg->titleLength);
+            break;
+        default:
+            PRINTF("Unhandled selector Index: %d\n", context->selectorIndex);
+            msg->result = ETH_PLUGIN_RESULT_ERROR;
+            return;
+    }
+    // amountToString(context->amount_sent,
+    //                sizeof(context->amount_sent),
+    //                context->decimals_sent,
+    //                context->ticker_sent,
+    //                msg->msg,
+    //                msg->msgLength);
 }
-
-// Set UI for "Token" screen.
-static void set_token_ui(ethQueryContractUI_t *msg, context_t *context) {
-    strlcpy(msg->title, "Token", msg->titleLength);
+// Set UI for "Receive" screen.
+static void set_receive_ui(ethQueryContractUI_t *msg, poap_parameters_t *context) {
+    switch (context->selectorIndex) {
+        case MINT_TOKEN:
+            strlcpy(msg->title, "Token", msg->titleLength);
+            break;
+        default:
+            PRINTF("Unhandled selector Index: %d\n", context->selectorIndex);
+            msg->result = ETH_PLUGIN_RESULT_ERROR;
+            return;
+    }
 
     amountToString(context->token_received,
                    sizeof(context->token_received),
                    context->decimals,
-                   context->ticker,
+                   context->ticker_received,
                    msg->msg,
                    msg->msgLength);
 }
 
 // Set UI for "Beneficiary" screen.
-static void set_beneficiary_ui(ethQueryContractUI_t *msg, context_t *context) {
+static void set_beneficiary_ui(ethQueryContractUI_t *msg, poap_parameters_t *context) {
     strlcpy(msg->title, "Beneficiary", msg->titleLength);
 
     msg->msg[0] = '0';
     msg->msg[1] = 'x';
 
-    getEthAddressStringFromBinary(context->beneficiary, msg->msg + 2, msg->pluginSharedRW->sha3, 0);
+    getEthAddressStringFromBinary((uint8_t *) context->beneficiary,
+                                  (uint8_t *) msg->msg + 2,
+                                  msg->pluginSharedRW->sha3,
+                                  0);
 }
 
 // Set UI for "Warning" screen.
 static void set_warning_ui(ethQueryContractUI_t *msg,
-                           const context_t *context __attribute__((unused))) {
+                           const poap_parameters_t *context __attribute__((unused))) {
     strlcpy(msg->title, "WARNING", msg->titleLength);
     strlcpy(msg->msg, "Unknown token", msg->msgLength);
 }
 
 // Helper function that returns the enum corresponding to the screen that should be displayed.
-static screens_t get_screen(const ethQueryContractUI_t *msg, const context_t *context) {
+static screens_t get_screen(const ethQueryContractUI_t *msg, const poap_parameters_t *context) {
     uint8_t index = msg->screenIndex;
 
     bool token_received_found = context->tokens_found & TOKEN_RECEIVED_FOUND;
-    bool token_received_not_found = !token_received_found;
+
     switch (index) {
         case 0:
-            if (token_received_found) {
-                return TOKEN_SCREEN;
-            } else if (token_received_not_found) {
-                return WARN_SCREEN;
-            }
+            return MINT_SCREEN;
             break;
         case 1:
-            if (token_received_found) {
-                return BENEFICIARY_SCREEN;
-            } else if (token_received_not_found) {
-                return WARN_SCREEN;
-            }
+            return BENEFICIARY_SCREEN;
+            break;
+        case 2:
+            return TOKEN_SCREEN;
+            break;
+        case 3:
+            return WARN_SCREEN;
             break;
         default:
             return ERROR;
@@ -64,25 +85,24 @@ static screens_t get_screen(const ethQueryContractUI_t *msg, const context_t *co
 
 void handle_query_contract_ui(void *parameters) {
     ethQueryContractUI_t *msg = (ethQueryContractUI_t *) parameters;
-    context_t *context = (context_t *) msg->pluginContext;
+    poap_parameters_t *context = (poap_parameters_t *) msg->pluginContext;
 
     memset(msg->title, 0, msg->titleLength);
     memset(msg->msg, 0, msg->msgLength);
     msg->result = ETH_PLUGIN_RESULT_OK;
 
     screens_t screen = get_screen(msg, context);
-
     switch (screen) {
         case MINT_SCREEN:
-            set_mint_ui(msg, context);
+            set_send_ui(msg, context);
             break;
         case TOKEN_SCREEN:
-            set_token_ui(msg, context);
-            break;
-        case BENEFICIARY_SCREEN:
-            set_beneficiary_ui(msg, context);
+            set_receive_ui(msg, context);
             break;
         case WARN_SCREEN:
+            set_beneficiary_ui(msg, context);
+            break;
+        case BENEFICIARY_SCREEN:
             set_warning_ui(msg, context);
             break;
         default:
--- src/poap_plugin.h
+++ src/poap_plugin.h
@@ -4,29 +4,23 @@
 #include "eth_plugin_interface.h"
 #include <string.h>
 
-#define NUM_SELECTORS        1
-#define PLUGIN_NAME          "Poap"
+#define PARAMETER_LENGTH 32
+#define SELECTOR_SIZE    4
+
+#define RUN_APPLICATION 1
+
+#define NUM_POAP_SELECTORS 1
+#define SELECTOR_SIZE          4
+
+#define PLUGIN_NAME "Poap"
+
 #define TOKEN_RECEIVED_FOUND 1 << 1
-#define SELECTOR_SIZE        4
-#define PARAMETER_LENGTH     32
-#define RUN_APPLICATION      1
 
-// Number of decimals used when the token wasn't found in the CAL.
-#define DEFAULT_DECIMAL WEI_TO_ETHER
-
-// Ticker used when the token wasn't found in the CAL.
-#define DEFAULT_TICKER ""
 typedef enum {
-    MINT_TOKEN,
-} selector_t;
+    MINT_TOKEN
+} poapSelector_t;
 
-// Enumeration used to parse the smart contract data.
-typedef enum {
-    EVENT_ID,
-    TOKEN_RECEIVED,
-    BENEFICIARY,
-    NONE,
-} parameter;
+extern const uint8_t *const POAP_SELECTORS[NUM_POAP_SELECTORS];
 
 typedef enum {
     MINT_SCREEN,
@@ -36,34 +30,40 @@ typedef enum {
     ERROR,
 } screens_t;
 
-extern const uint8_t *const POAP_SELECTORS[NUM_SELECTORS];
+// Would've loved to make this an enum but we don't have enough room because enums are `int` and not
+// `uint8_t`.
+#define EVENT_ID       0  
+#define TOKEN_RECEIVED 1 
+#define BENEFICIARY    2 
+#define NONE           3
+
+// Number of decimals used when the token wasn't found in the CAL.
+#define DEFAULT_DECIMAL WEI_TO_ETHER
+
+// Ticker used when the token wasn't found in the CAL.
+#define DEFAULT_TICKER ""
 
 // Shared global memory with Ethereum app. Must be at most 5 * 32 bytes.
-typedef struct context_t {
-    // For display.
-    uint8_t beneficiary[ADDRESS_LENGTH];
-    uint8_t token_received[ADDRESS_LENGTH];
-    char ticker[MAX_TICKER_LEN];
-    uint8_t decimals;
+typedef struct poap_parameters_t {
+    uint8_t token_received[INT256_LENGTH];
+    char beneficiary[ADDRESS_LENGTH];
+    char ticker_received[MAX_TICKER_LEN];
 
-    // For parsing data.
     uint16_t offset;
     uint16_t checkpoint;
-    uint8_t skip;
     uint8_t next_param;
     uint8_t tokens_found;
+    uint8_t valid;
+    uint8_t decimals;
+    uint8_t selectorIndex;
+    uint8_t array_len;
+    uint8_t skip;
+    // 4 * 1 + 2 * 2 + 7 * 1 == 8 + 7 == 15 bytes. There are 16 - 15 == 1 byte left.
+} poap_parameters_t;
 
-    // For both parsing and display.
-    selector_t selectorIndex;
-} context_t;
-
-// Piece of code that will check that the above structure is not bigger than 5 * 32. Do not remove
-// this check.
-_Static_assert(sizeof(context_t) <= 5 * 32, "Structure of parameters too big.");
-
+void handle_init_contract(void *parameters);
 void handle_provide_parameter(void *parameters);
 void handle_query_contract_ui(void *parameters);
-void handle_init_contract(void *parameters);
 void handle_finalize(void *parameters);
 void handle_provide_token(void *parameters);
 void handle_query_contract_id(void *parameters);
