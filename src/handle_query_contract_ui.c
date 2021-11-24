53ba09797407ef3158bfec32936bea68b65126cb
lisaoulmi <lisa@blooo.io>
2021-11-24 10:09:15 +0000

    test: removed useless comments

--- src/handle_query_contract_ui.c
+++ src/handle_query_contract_ui.c
@@ -11,12 +11,6 @@ static void set_send_ui(ethQueryContractUI_t *msg, poap_parameters_t *context) {
             msg->result = ETH_PLUGIN_RESULT_ERROR;
             return;
     }
-    // amountToString(context->amount_sent,
-    //                sizeof(context->amount_sent),
-    //                context->decimals_sent,
-    //                context->ticker_sent,
-    //                msg->msg,
-    //                msg->msgLength);
 }
 // Set UI for "Receive" screen.
 static void set_receive_ui(ethQueryContractUI_t *msg, poap_parameters_t *context) {
@@ -100,10 +94,10 @@ void handle_query_contract_ui(void *parameters) {
             set_receive_ui(msg, context);
             break;
         case WARN_SCREEN:
-            set_beneficiary_ui(msg, context);
+            set_warning_ui(msg, context);
             break;
         case BENEFICIARY_SCREEN:
-            set_warning_ui(msg, context);
+            set_beneficiary_ui(msg, context);
             break;
         default:
             PRINTF("Received an invalid screenIndex\n");
